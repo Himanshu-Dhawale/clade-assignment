253c78e84ba3475808dbe8dec72c83abfa39ada5
Himanshu <himanshudhawale9@gmail.com>
2024-06-26 14:04:31 +0530

    [feat: Added sidebar]

---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Sidebar from '../components/Sidebar'
+
+const LandingPage = () => {
+  return (
+    <div className='w-full min-h-screen bg-[#fcfcfc]'>
+      LandingPage
+     <Sidebar/>
+    </div>
+  )
+}
+
+export default LandingPage
