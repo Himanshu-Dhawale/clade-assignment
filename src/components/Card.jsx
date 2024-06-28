13a004aab76d99abcd5187d1e5c5169dd29efa8c
Himanshu <himanshudhawale9@gmail.com>
2024-06-28 12:06:40 +0530

    [fix: Sidebar and Scrollbar issue]

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,7 +6,7 @@ const Card = () => {
         <h2 className='font-normal text-xl leading-[30px]'>“A quote from a Atlassian.”</h2>
 
         <div className='w-auto max-w-[153px] h-auto max-h-[50px] flex gap-4'>
-           <img src="src\assets\Avatar.png" alt="Avatar" className='w-11 h-11'/>
+           <img src="Avatar.png" alt="Avatar" className='w-11 h-11'/>
            <div className='w-auto max-w-[92px] h-auto max-h-[50px] flex gap-[2px] flex-col'>
                 <h2 className='font-medium text-base leading-6'>Name</h2>
                 <p className='text-[#828282] font-medium text-base leading-6'>Description</p>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -55,7 +55,7 @@ const Navbar = () => {
           <div className="flex gap-4 max-w-[683px] h-[78.85px]">
             <button className="w-[109px] h-[58px] flex items-center justify-center self-center gap-2 bg-[#DC4A2D] rounded-[49px]">
               <img
-                src="src\assets\briefcase-02.png"
+                src="briefcase-02.png"
                 alt="briefcase"
                 className="w-6 h-6"
               />
@@ -63,16 +63,16 @@ const Navbar = () => {
             </button>
             <div className="max-h-[74px] px-6 flex gap-10 justify-center items-center border-[0.5px] border-[#D1D1D1] rounded-[36px] shadow-[0px_4px_4px_0px_#00000040_inset] sm:px-10 md:px-[104px]">
               <div className="flex gap-2 items-center text-[#B0B0B0]">
-                <img src="src\assets\message-square-01.png" alt="messages" />
+                <img src="message-square-01.png" alt="messages" />
                 <span>Messages</span>
               </div>
               <div className="flex gap-6">
                 <div className="flex gap-2 items-center text-[#B0B0B0]">
-                  <img src="src\assets\coins-hand.png" alt="coins" />
+                  <img src="coins-hand.png" alt="coins" />
                   <span>Payments</span>
                 </div>
                 <div className="flex gap-2 items-center text-[#B0B0B0]">
-                  <img src="src\assets\users-01.png" alt="users" />
+                  <img src="users-01.png" alt="users" />
                   <span>Application</span>
                 </div>
               </div>
@@ -80,10 +80,10 @@ const Navbar = () => {
           </div>
           {/* right side */}
           <div className="flex gap-4 items-center">
-            <img src="src\assets\bell-02.png" alt="bell" className="w-[23.3px] h-[25.33px]"/>
+            <img src="bell-02.png" alt="bell" className="w-[23.3px] h-[25.33px]"/>
             <div className="flex items-center gap-[6px]">
-              <img src="src\assets\Rectangle 5 (1).png" alt="profile" className="w-[40px] h-[40px]"/>
-              <img src="src\assets\chevron-down.png" alt="arrow" className="w-[20px] h-[20px]"/>
+              <img src="Rectangle 5 (1).png" alt="profile" className="w-[40px] h-[40px]"/>
+              <img src="chevron-down.png" alt="arrow" className="w-[20px] h-[20px]"/>
             </div>
           </div>
         </div>
